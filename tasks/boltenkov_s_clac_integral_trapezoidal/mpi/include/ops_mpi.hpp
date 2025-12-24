1913db14f096fb23e5933e76b2eae7efaceb40e6
theProgrammerOfOurTime <azarova.v2104@gmail.com>
2025-12-24 09:35:54 +0300

    fix clang-format

--- tasks/boltenkov_s_clac_integral_trapezoidal/mpi/include/ops_mpi.hpp
+++ tasks/boltenkov_s_clac_integral_trapezoidal/mpi/include/ops_mpi.hpp
@@ -12,14 +12,14 @@ class BoltenkovSCalcIntegralkMPI : public BaseTask {
   static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
     return ppc::task::TypeOfTask::kMPI;
   }
-  explicit BoltenkovSCalcIntegralkMPI(const InType& in);
+  explicit BoltenkovSCalcIntegralkMPI(const InType &in);
 
  private:
   int sign_integral_;
-  double CalcCoef(const std::vector<double>& args, const std::vector<std::pair<double, double>>& limits);
-  void CalcPoints(const int& n, const int& ind_cur_args, const std::vector<double>& h,
-                  std::unique_ptr<std::queue<std::vector<double>>>& args);
-  double calcIntegral(const int& n, const int& cnt_limits, const std::vector<std::pair<double, double>>& limits,
+  double CalcCoef(const std::vector<double> &args, const std::vector<std::pair<double, double>> &limits);
+  void CalcPoints(const int &n, const int &ind_cur_args, const std::vector<double> &h,
+                  std::unique_ptr<std::queue<std::vector<double>>> &args);
+  double calcIntegral(const int &n, const int &cnt_limits, const std::vector<std::pair<double, double>> &limits,
                       double (*func)(std::vector<double>));
   bool ValidationImpl() override;
   bool PreProcessingImpl() override;
